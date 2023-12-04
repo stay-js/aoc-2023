8580ce330329e9f43a42c9d7dfce13dcafecb665
Nagy Zétény <nagyzeteny6@gmail.com>
2023-12-04 18:24:55 +0100

    day 4 hashset

--- 04/main.rs
+++ 04/main.rs
@@ -1,17 +1,19 @@
-fn parse_line(line: &str) -> (Vec<u32>, Vec<u32>) {
+use std::collections::HashSet;
+
+fn parse_line(line: &str) -> (HashSet<u32>, HashSet<u32>) {
     let data: Vec<&str> = line.split(": ").nth(1).unwrap().split(" | ").collect();
 
-    let winning_numbers = data[0]
+    let winning_numbers: HashSet<u32> = data[0]
         .split(" ")
         .filter(|&x| x != "")
         .map(|x| x.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
+        .collect();
 
-    let numbers = data[1]
+    let numbers: HashSet<u32> = data[1]
         .split(" ")
         .filter(|&x| x != "")
         .map(|x| x.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
+        .collect();
 
     return (winning_numbers, numbers);
 }
@@ -22,19 +24,11 @@ fn first_part(input: &String) {
     for line in input.lines() {
         let (winning_numbers, numbers) = parse_line(line);
 
-        let mut game_total = 0;
+        let winning = winning_numbers.intersection(&numbers).count();
 
-        for number in numbers {
-            if winning_numbers.contains(&number) {
-                if game_total == 0 {
-                    game_total = 1;
-                } else {
-                    game_total *= 2;
-                }
-            }
+        if winning != 0 {
+            total += u32::pow(2, winning as u32 - 1);
         }
-
-        total += game_total;
     }
 
     println!("First part: {}", total);
@@ -47,15 +41,7 @@ fn second_part(input: &String) {
     for (i, line) in input.lines().enumerate() {
         let (winning_numbers, numbers) = parse_line(line);
 
-        let mut game_total = 0;
-
-        for number in numbers {
-            if winning_numbers.contains(&number) {
-                game_total += 1;
-            }
-        }
-
-        let mut max = i + game_total + 1;
+        let mut max = i + 1 + winning_numbers.intersection(&numbers).count();
 
         if max > copies.len() {
             max = copies.len();
