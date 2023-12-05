dd1f975cd5c68f7f51c146c5d3bb304dd70ea48f
Nagy Zétény <nagyzeteny6@gmail.com>
2023-12-05 16:08:24 +0100

    day 5 part 1

--- 05/main.rs
+++ 05/main.rs
@@ -4,26 +4,12 @@ struct Map {
     length: u64,
 }
 
-struct Data {
-    seeds: Vec<u64>,
-    seed_to_soil_map: Vec<Map>,
-    soil_to_fertilizer: Vec<Map>,
-    fertilizer_to_water: Vec<Map>,
-    water_to_light: Vec<Map>,
-    light_to_temperature: Vec<Map>,
-    temperature_to_humidity: Vec<Map>,
-    humidity_to_location: Vec<Map>,
-}
-
 fn parse_item(item: &str) -> Vec<Map> {
     return item
         .lines()
         .skip(1)
         .map(|line| {
-            let data = line
-                .split(" ")
-                .map(|x| x.parse::<u64>().unwrap())
-                .collect::<Vec<u64>>();
+            let data: Vec<u64> = line.split(" ").map(|x| x.parse::<u64>().unwrap()).collect();
 
             return Map {
                 destination_min: data[0],
@@ -34,34 +20,11 @@ fn parse_item(item: &str) -> Vec<Map> {
         .collect();
 }
 
-fn get_data(input: &String) -> Data {
-    let data = input.split("\n\n").collect::<Vec<&str>>();
-
-    let seeds = data[0]
-        .split(": ")
-        .nth(1)
-        .unwrap()
-        .split(" ")
-        .map(|x| x.parse::<u64>().unwrap())
-        .collect::<Vec<u64>>();
-
-    return Data {
-        seeds,
-        seed_to_soil_map: parse_item(data[1]),
-        soil_to_fertilizer: parse_item(data[2]),
-        fertilizer_to_water: parse_item(data[3]),
-        water_to_light: parse_item(data[4]),
-        light_to_temperature: parse_item(data[5]),
-        temperature_to_humidity: parse_item(data[6]),
-        humidity_to_location: parse_item(data[7]),
-    };
-}
-
-fn map_to_next(current: Vec<u64>, next: &Vec<Map>) -> Vec<u64> {
+fn map_to_next(current: Vec<u64>, next: Vec<Map>) -> Vec<u64> {
     return current
         .iter()
         .map(|item| {
-            for map in next {
+            for map in next.iter() {
                 if map.source_min <= *item && *item < map.source_min + map.length {
                     return map.destination_min + (*item - map.source_min);
                 }
@@ -73,44 +36,28 @@ fn map_to_next(current: Vec<u64>, next: &Vec<Map>) -> Vec<u64> {
 }
 
 fn first_part(input: &String) {
-    let data = get_data(input);
+    let data: Vec<&str> = input.split("\n\n").collect();
 
-    let soils = map_to_next(data.seeds, &data.seed_to_soil_map);
-    let fertilizers = map_to_next(soils, &data.soil_to_fertilizer);
-    let waters = map_to_next(fertilizers, &data.fertilizer_to_water);
-    let lights = map_to_next(waters, &data.water_to_light);
-    let temperatures = map_to_next(lights, &data.light_to_temperature);
-    let humidities = map_to_next(temperatures, &data.temperature_to_humidity);
-    let locations = map_to_next(humidities, &data.humidity_to_location);
-
-    println!("First part: {}", locations.iter().min().unwrap());
-}
-
-fn second_part(input: &String) {
-    let data = get_data(input);
-
-    let seeds: Vec<u64> = data
-        .seeds
-        .chunks(2)
-        .flat_map(|x| x[0]..x[0] + x[1])
+    let mut current = data[0]
+        .split(": ")
+        .nth(1)
+        .unwrap()
+        .split(" ")
+        .map(|x| x.parse::<u64>().unwrap())
         .collect();
 
-    let soils = map_to_next(seeds, &data.seed_to_soil_map);
-    let fertilizers = map_to_next(soils, &data.soil_to_fertilizer);
-    let waters = map_to_next(fertilizers, &data.fertilizer_to_water);
-    let lights = map_to_next(waters, &data.water_to_light);
-    let temperatures = map_to_next(lights, &data.light_to_temperature);
-    let humidities = map_to_next(temperatures, &data.temperature_to_humidity);
-    let locations = map_to_next(humidities, &data.humidity_to_location);
+    for i in 1..data.len() {
+        current = map_to_next(current, parse_item(data[i]));
+    }
 
-    println!("Second part: {}", locations.iter().min().unwrap());
+    println!("First part: {}", current.iter().min().unwrap());
 }
 
 fn main() {
     println!("demo-input.txt:");
     let input = std::fs::read_to_string("./05/demo-input.txt").unwrap();
     first_part(&input);
-    second_part(&input);
+    // second_part(&input);
 
     println!("\ninput.txt:");
     let input = std::fs::read_to_string("./05/input.txt").unwrap();
