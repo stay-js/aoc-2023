c4e90f6ba05294d1bc7e020572418d8f1c78d978
Nagy Zétény <nagyzeteny6@gmail.com>
2023-12-06 15:58:07 +0100

    day 6 part 2

--- 06/main.rs
+++ 06/main.rs
@@ -18,6 +18,7 @@ fn first_part(input: &String) {
         .collect();
 
     let mut total: u32 = 1;
+
     for (time, distance) in time.iter().zip(distance.iter()) {
         let mut possibilities: u32 = 0;
 
@@ -33,14 +34,46 @@ fn first_part(input: &String) {
     println!("First part: {}", total);
 }
 
+fn second_part(input: &String) {
+    let data: Vec<&str> = input.lines().collect();
+
+    let time: u64 = data[0]
+        .split(":")
+        .nth(1)
+        .unwrap()
+        .split_whitespace()
+        .collect::<String>()
+        .parse()
+        .unwrap();
+
+    let distance: u64 = data[1]
+        .split(":")
+        .nth(1)
+        .unwrap()
+        .split_whitespace()
+        .collect::<String>()
+        .parse()
+        .unwrap();
+
+    let mut possibilities: u64 = 0;
+
+    for i in 0..time {
+        if (time - i) * i > distance {
+            possibilities += 1;
+        }
+    }
+
+    println!("Second part: {}", possibilities);
+}
+
 fn main() {
     println!("demo-input.txt:");
     let input = std::fs::read_to_string("./06/demo-input.txt").unwrap();
     first_part(&input);
-    // second_part(&input);
+    second_part(&input);
 
     println!("\ninput.txt:");
     let input = std::fs::read_to_string("./06/input.txt").unwrap();
     first_part(&input);
-    // second_part(&input);
+    second_part(&input);
 }
